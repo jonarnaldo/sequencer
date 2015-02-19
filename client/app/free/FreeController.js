4ca5511f1988487b082e53ac27dd55aab2e2c0c5
jonarnaldo <jonathanarnaldo@Jonathans-MacBook-Air.local>
2015-02-19 15:46:20 -0800

    adds sounds

--- client/app/free/FreeController.js
+++ client/app/free/FreeController.js
@@ -12,60 +12,30 @@
     var vm = this;
     vm.seqArray = [];
 
-    vm.createSeqArray = function(rows, columns) {
-      //create rows
-      for (var i = 0; i < rows; i++) {
-        vm.seqArray.push([]);
-        for (var j = 0; j < columns; j++) {
-          vm.seqArray[i].push({});
-        }
+    // for testing - adds 'beep' to each unit
+    vm.click = function(object, rowIndex, columnIndex) {
+      console.log('clicked!', rowIndex, columnIndex);
+      FreeFactory.findUnit(vm.seqArray, rowIndex, columnIndex, function(object) {
+        object.sound = "sound";
+      });
+      FreeFactory.play();
+    }
+
+
+    vm.playSequence = function() {
+      // goes through sequenceArray rows and invoke cycleColumns simultaneously
+      // for each row...
+      for (var i = 0; i < vm.seqArray.length; i++) {
+        console.log(vm.seqArray[i]);
       }
+    }   
+
+    function init() {
+      FreeFactory.createSeqArray(vm.seqArray, 3, 3);
       console.log(vm.seqArray);
     }
 
-
-    function timeout(object, index, time) {
-      $timeout(function() {
-        console.log(object);
-      }, (index + 1) * time);
-    }
-
-    // cycles through a single row's columns
-    vm.cycleColumns = function(row) {
-      var row = vm.seqArray[row];
-      for (var i = 0; i < row.length; ++i) {
-        timeout(row[i], i, 2000);
-      }
-    }
-
-    // cycles through entire array
-    vm.cycleSeqArray = function(cb) {
-      angular.forEach(vm.seqArray, function(row, rowIndex) {
-        angular.forEach(row, function(col, colIndex) {
-          cb(rowIndex, colIndex, col);
-        })
-      })
-    }
-
-    // for testing - adds 'beep' to each unit
-    vm.beep = function(object, rIndex, cIndex) {
-      vm.cycleSeqArray(function(rowIndex, colIndex, col) {
-        if (rowIndex === rIndex && colIndex === cIndex) { col.beep = object; }
-      })
-      console.log(vm.seqArray)
-    }
-
-    vm.playSequence = function() {
-      // goes through sequenceArray rows and fired cycleColumns simultaneously
-      // for each row... 
-      vm.cycleColumns(0);
-    }
-
-    vm.init = function() {
-      vm.createSeqArray(3,3);
-    }
-
-    vm.init();
+    init();
   }
 })();
     
--- client/app/free/FreeFactory.js
+++ client/app/free/FreeFactory.js
@@ -7,6 +7,60 @@
   function FreeFactory($http, $q, $timeout){
 
     var services = {
+      play: play,
+      createSeqArray: createSeqArray,
+      cycleColumns: cycleColumns,
+      findUnit: findUnit,
+      timeout: timeout
+    }
+
+
+    ion.sound({
+      sounds: [
+        {
+          name: "test",
+          volume: 0.5,
+          preload: false
+        }
+      ],
+      volume: 0.5,
+      path: "sounds/",
+      preload: true
+    });
+
+    function play () {
+      console.log('playing test');
+      ion.sound.play("test");
+    }
+
+    function createSeqArray(array, rows, columns) {
+      //create rows
+      for (var i = 0; i < rows; i++) {
+        array.push([]);
+        for (var j = 0; j < columns; j++) {
+          array[i].push({});
+        }
+      }
+    }
+
+    function findUnit(array, r, c, cb) {
+      cb(array[r][c]);
+    }
+
+    // cycles through a single row's columns
+    function cycleColumns(row, time, cb) {
+      console.log(row);
+      for (var i = 0; i < row.length; i++) {
+        timeout(row[i], i, time, function() {
+          cb();
+        });
+      }
+    } 
+
+    function timeout(object, index, time, cb) {
+      $timeout(function() {
+        cb();
+      }, index * time);
     }
 
     return services;
